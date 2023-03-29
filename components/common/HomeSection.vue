1acdb37ecca39228925475b60277fcd8ccc2b08f
woddy <turincev_av12@mail.ru>
2023-03-29 23:47:20 +0500

    fix

--- components/common/HomeSection.vue
+++ components/common/HomeSection.vue
@@ -4,12 +4,12 @@
      <div class="main">
        <span class="white">UCAD</span>
        <span class="dot">.</span>
-       <span class="blue">TEAM</span>
+       <span class="blue">TEAM__</span>
      </div>
      <div class="additional">
        <span class="white" data-text="UCAD">UCAD</span>
        <span class="dot" data-text=".">.</span>
-       <span class="blue" data-text=" TEAM">TEAM_</span>
+       <span class="blue" data-text=" TEAM">TEAM__</span>
      </div>
    </div>
    <div class="home-section__background"/>
