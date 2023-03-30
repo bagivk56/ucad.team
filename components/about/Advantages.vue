edbb8a7b7ebd5b23fd2c7502e81584b245d07dfd
woddy <turincev_av12@mail.ru>
2023-03-30 22:16:21 +0500

    html blocks

--- components/about/Advantages.vue
+++ components/about/Advantages.vue
@@ -1,10 +1,24 @@
 <template>
- <div class="advantages-section custom-section metal"></div>
+ <div class="advantages-section custom-section metal">
+   <div class="container">
+     <h2 class="custom-section__title">Преимущества</h2>
+     <div class="advantages__list">
+       <AdvantageCard/>
+       <AdvantageCard/>
+       <AdvantageCard/>
+       <AdvantageCard/>
+     </div>
+   </div>
+ </div>
 </template>
 
 <script>
 export default {
-  name: "Advantages"
+  name: "Advantages",
+
+  components: {
+    AdvantageCard: () => import("~/components/about/_AdvantageCard")
+  }
 }
 </script>
 
@@ -13,5 +27,17 @@ export default {
   padding: 180px 0;
   box-sizing: border-box;
   background: linear-gradient(90.66deg, #161616 0.57%, #160C1B 25.09%, #160313 62.84%, #000000 99.49%);
+}
+.advantages__list {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: -50px;
+  margin-left: -30px;
+
+  & > * {
+    width: calc(100% / 4 - 30px);
+    margin-top: 50px;
+    margin-left: 30px;
+  }
 }
 </style>
---
+++ components/about/_AdvantageCard.vue
@@ -0,0 +1,52 @@
+<template>
+ <div class="advantage-card">
+   <div class="advantage-card__head">
+     <div class="advantage-card__icon">
+       <span/>
+     </div>
+     <div class="title">
+       Умеем<br/>слышать Вас
+     </div>
+   </div>
+   <div class="advantage-card__message cms-block-stub">
+     В каждой сфере бизнеса есть проблемы, которые необходимо преодолеть, умение совмещать пожелания заказчика и улучшать показатели метрик – самый главный критерий выполненной работы.
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "AdvantageCard"
+}
+</script>
+
+<style lang="scss" scoped>
+.advantage-card {}
+.advantage-card__head {
+  display: flex;
+  margin-bottom: 20px;
+  .title {
+    font-family: 'Steppe';
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 24px;
+    color: #087AFF;
+  }
+}
+.advantage-card__icon {
+  width: 44px;
+  height: 44px;
+  border-radius: 100%;
+  background-color: #4209B0;
+  margin-right: 18px;
+
+  span {
+    display: flex;
+    width: 14px;
+    height: 14px;
+    border-radius: 100%;
+    background-color: #FFFFFF;
+  }
+}
+.advantage-card__message {}
+</style>
