e94854c6529a6e6fa2dd0ef61103c82126c1339b
woddy <turincev_av12@mail.ru>
2023-04-06 23:25:29 +0500

    fix

---
+++ components/common/LeftSideBarScroll.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="left-side-bar-scroll">
+    <div class="scroll">
+      <span />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeftSideBarScroll"
+}
+</script>
+
+<style lang="scss" scoped>
+.left-side-bar-scroll {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 60px 0;
+  box-sizing: border-box;
+  position: fixed;
+  height: 100vh;
+  width: 70px;
+  top: 0; left: 0;
+  z-index: 30;
+}
+
+.scroll {
+  width: 1px;
+  height: 100%;
+  background-color: #585858;
+
+  span {
+    display: flex;
+    width: 6px;
+    height: 46px;
+    border: 1px solid #FFFFFF;
+    border-radius: 10px;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    cursor: pointer;
+    transition: all 0.2s;
+
+    &:hover {
+      width: 16px;
+      background: linear-gradient(180deg, #003471 0%, #5644F7 48.75%, #A80CEE 100%);
+      position: relative;
+      border: none;
+      &:after {
+        content: "";
+        position: absolute;
+        top: 3px; left: 3px; right: 3px; bottom: 3px;
+        border-radius: 10px;
+        background-color: black;
+      }
+    }
+  }
+}
+</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,17 +1,11 @@
 <template>
- <div>
-   <section class="fullpage">
-     <AboutProject/>
-   </section>
-   <section class="fullpage">
-     <Team/>
-   </section>
-   <section class="fullpage">
-     <Advantages/>
-   </section>
-   <section class="fullpage">
-     <FormFeedback/>
-   </section>
+ <div class="relative">
+   <AboutProject/>
+   <Team/>
+   <Advantages/>
+   <FormFeedback/>
+
+   <LeftSideBarScroll/>
  </div>
 </template>
 
@@ -23,128 +17,17 @@ import FormFeedback from "~/components/about/FormFeedback";
 export default {
   name: "index",
 
-  data: function () {
-    return {
-      inMove: false,
-      inMoveDelay: 400,
-      activeSection: 0,
-      offsets: [],
-      touchStartY: 0
-    }
-  },
+  data: function () {},
 
   components: {
     AboutProject,
     Team,
 
     Advantages: () => import("~/components/about/Advantages"),
-    FormFeedback
+    FormFeedback,
+
+    LeftSideBarScroll: () => import("~/components/common/LeftSideBarScroll")
   },
-
-  mounted: function () {
-    // this.calculateSectionOffsets();
-    //
-    // window.addEventListener('DOMMouseScroll', this.handleMouseWheelDOM);  // Mozilla Firefox
-    // window.addEventListener('mousewheel', this.handleMouseWheel, { passive: false }); // Other browsers
-    // window.addEventListener('touchstart', this.touchStart, { passive: false }); // mobile devices
-    // window.addEventListener('touchmove', this.touchMove, { passive: false }); // mobile devices
-  },
-  destroyed: function () {
-    window.removeEventListener('DOMMouseScroll', this.handleMouseWheelDOM); // Mozilla Firefox
-    window.removeEventListener('mousewheel', this.handleMouseWheel, { passive: false });  // Other browsers
-
-    window.removeEventListener('touchstart', this.touchStart); // mobile devices
-    window.removeEventListener('touchmove', this.touchMove); // mobile devices
-  },
-
-  methods: {
-    calculateSectionOffsets() {
-      let sections = document.getElementsByTagName('section');
-      let length = sections.length;
-
-      for(let i = 0; i < length; i++) {
-        let sectionOffset = sections[i].offsetTop;
-        this.offsets.push(sectionOffset);
-      }
-    },
-    handleMouseWheel: function(e) {
-
-      if (e.wheelDelta < 30 && !this.inMove) {
-        this.moveUp();
-      } else if (e.wheelDelta > 30 && !this.inMove) {
-        this.moveDown();
-      }
-
-      e.preventDefault();
-      return false;
-    },
-    handleMouseWheelDOM: function(e) {
-
-      if (e.detail > 0 && !this.inMove) {
-        this.moveUp();
-      } else if (e.detail < 0 && !this.inMove) {
-        this.moveDown();
-      }
-
-      return false;
-    },
-
-    moveUp() {
-      this.inMove = true;
-      this.activeSection++;
-      if(this.activeSection > this.offsets.length - 1) this.activeSection = 0;
-      this.scrollToSection(this.activeSection, true);
-    },
-    moveDown() {
-      this.inMove = true;
-      this.activeSection--;
-
-      if(this.activeSection < 0) {
-        return
-      };
-      this.activeSection = this.offsets.length - 1
-
-      console.log('this.activeSection: ', this.activeSection);
-      this.scrollToSection(this.activeSection, true);
-    },
-
-    scrollToSection(id, force = false) {
-      if(this.inMove && !force) return false;
-
-      this.activeSection = id;
-      this.inMove = true;
-
-      // get section and scroll into view if it exists
-      let section = document.getElementsByTagName('section')[id];
-      if(section) {
-        document.getElementsByTagName('section')[id].scrollIntoView({behavior: 'smooth'});
-      }
-
-      setTimeout(() => {
-        this.inMove = false;
-      }, this.inMoveDelay);
-    },
-    touchStart(e) {
-      e.preventDefault();
-
-      this.touchStartY = e.touches[0].clientY;
-    },
-    touchMove(e) {
-      if(this.inMove) return false;
-      e.preventDefault();
-
-      const currentY = e.touches[0].clientY;
-
-      if(this.touchStartY < currentY) {
-        this.moveDown();
-      } else {
-        this.moveUp();
-      }
-
-      this.touchStartY = 0;
-      return false;
-    }
-  }
 }
 </script>
 
