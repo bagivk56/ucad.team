974d20d6044bf221ac90e41fddadc1e4852b2ec5
woddy <turincev_av12@mail.ru>
2023-05-01 21:06:16 +0500

    fix

--- components/advantages/CardMobile.vue
+++ components/advantages/CardMobile.vue
@@ -62,6 +62,7 @@ export default {
   font-size: 18px;
   line-height: 18px;
   background: linear-gradient(to right, rgba(8,122,255,1) 0%, rgba(168,12,238,1) 50%, rgba(168,12,238,1) 75%, rgba(8,122,255,1) 100%);
+  -webkit-mask-image: linear-gradient(to right, rgba(8,122,255,1) 0%, rgba(168,12,238,1) 50%, rgba(168,12,238,1) 75%, rgba(8,122,255,1) 100%);
   background-size: 200% auto;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
